77293e9ff10aad6f29e03cfbaaea3cee48cc9cc3
rosveluz <112733712+rosveluz@users.noreply.github.com>
2023-10-12 01:03:11 +0800

    Mobile responsive fix

---
+++ responsive.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.createElement('div');
+    menuToggle.className = 'menu-toggle';
+    menuToggle.innerHTML = `<div></div><div></div><div></div>`;
+    document.querySelector('header').appendChild(menuToggle);
+
+    menuToggle.addEventListener('click', function() {
+        const nav = document.querySelector('nav');
+        nav.classList.toggle('active');
+    });
+});
